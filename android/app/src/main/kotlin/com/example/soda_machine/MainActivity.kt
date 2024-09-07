14595f55c57699fae974f791f317fdc179069bef
JohanScheepersf <scheepersjohanf@gmail.com>
2024-09-07 21:42:28 +0200

    Blank Project

---
+++ android/app/src/main/kotlin/com/example/soda_machine/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_raspberry_pi_iot
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const AppRun());
+}
+
+class AppRun extends StatelessWidget {
+  const AppRun({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Pi IOT',
+            style: TextStyle(fontWeight: FontWeight.w900),
+          ),
+          actions: const [
+            Text('soda_machine')
+          ],
+          backgroundColor: Colors.blueAccent,
+        ),
+        body: const Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('soda_machine')
+              
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

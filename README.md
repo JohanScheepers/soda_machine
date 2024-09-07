c511ba0d0b3c40783e7bf5b0789e8094f808e675
JohanScheepersf <scheepersjohanf@gmail.com>
2024-09-07 22:19:24 +0200

    Update app imagage, main.dart, app ID

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # soda_machine
+
+
+![main_menu_editor.png](https://https://github.com/JohanScheepers/soda_machine/tree/main/assets/images/pi/app.jpg)
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:soda_machine/main_app/widgets/time_size_widget.dart';
 
 void main() {
   runApp(const AppRun());
@@ -14,16 +15,30 @@ class AppRun extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: const Text(
-            'Pi IOT',
+            'soda_machine',
             style: TextStyle(fontWeight: FontWeight.w900),
           ),
-          actions: const [Text('soda_machine')],
+          actions: const [TimeSizeWidget()],
           backgroundColor: Colors.blueAccent,
         ),
         body: const Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [Text('soda_machine')],
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'soda_machine',
+                    style: TextStyle(fontSize: 32, fontWeight: FontWeight.w500),
+                  ),
+                  Image(
+                    image: AssetImage('assets/images/can_1.jpg'),
+                    fit: BoxFit.fitHeight,
+                  ),
+                ],
+              )
+            ],
           ),
         ),
       ),
